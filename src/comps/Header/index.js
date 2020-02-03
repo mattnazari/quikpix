790c94dc89d96d009ac50f487fc4d2f24886a65c
mattnazari <matthew.nazari8@gmail.com>
2020-02-02 17:42:26 -0800

    header (need to fix menu to row)

--- src/comps/Header/index.js
+++ src/comps/Header/index.js
@@ -3,12 +3,28 @@ import Menu from "../Menu";
 
 const Header = () => {
   return (
-    <div>
-      <Menu />
+    <div className="headerCont">
+      <div className="logo">QuikPix</div>
+      <Menu items={headerItems} />
     </div>
   );
 };
 
 Header.defaultProps = {};
+
+var headerItems = [
+  {
+    itemTitle: "Upgrade",
+    itemTitleColor: "#101010"
+  },
+  {
+    itemTitle: "Contact",
+    itemTitleColor: "#101010"
+  },
+  {
+    itemTitle: "Account",
+    itemTitleColor: "#101010"
+  }
+];
 
 export default Header;
