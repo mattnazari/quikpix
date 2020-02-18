b349b83d67347146a9b90fd55bdc8fd3adf3f345
mattnazari <matthew.nazari8@gmail.com>
2020-02-18 01:07:10 -0800

    plansCard setup

---
+++ src/comps/PlansCard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PlansCard = () => {
+  return (
+    <div>
+      <h1>hey</h1>
+    </div>
+  );
+};
+
+PlansCard.defaultProps = {};
+
+export default PlansCard;
