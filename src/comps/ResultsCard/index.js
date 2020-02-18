0d2aab01fd54e78533aceeb586bf43bdeed7ed33
mattnazari <matthew.nazari8@gmail.com>
2020-02-18 00:47:43 -0800

    results card ver1

--- src/comps/ResultsCard/index.js
+++ src/comps/ResultsCard/index.js
@@ -1,53 +1,68 @@
 import React from "react";
 import Button from "../Button";
 
-const ResultsCard = ({ innerImg, dimNum }) => {
+const ResultsCard = ({
+  innerImg,
+  dimNum,
+  resultsCardIcon,
+  resultsCardTitle
+}) => {
   return (
-    <div className="resultsCard">
-      <div className="leftCardSection">
-        <img className="innerImg" src={innerImg}></img>
-        <p className="dimNumTxt">Dimension: {dimNum} px</p>
+    <div className="resultsCardWrapper">
+      <div className="resultsCardTitlePosition">
+        <div className="resultsCardTitleSection">
+          <div className="innerResultsCard">
+            <img src={resultsCardIcon} className="resultsCardIcon" />
+            <h2 className="resultsCardTitle">{resultsCardTitle}</h2>
+          </div>
+        </div>
       </div>
-      <div className="rightCardSection">
-        <div className="copyInputTopCont">
-          <input
-            type="text"
-            placeholder="link.quikpix.converted..."
-            className="copyInput"
-          />
+      <div className="resultsCard">
+        <div className="leftCardSection">
+          <img className="innerImg" src={innerImg}></img>
+          <p className="dimNumTxt">Dimension: {dimNum} px</p>
+        </div>
+        <div className="rightCardSection">
+          <div className="copyInputTopCont">
+            <input
+              type="text"
+              placeholder="link.quikpix.converted..."
+              className="copyInput"
+            />
+            <Button
+              buttonWidth="73px"
+              buttonHeight="40px"
+              buttonBgColor="#31C1FF"
+              buttonTitle="Copy"
+              buttonFontSize="12px"
+              buttonRadius="10px"
+              buttonPosition="absolute"
+              buttonMarginL="-30px"
+            />
+          </div>
+          <div className="copyInputBotCont">
+            <input
+              type="text"
+              placeholder="<embed> </embed>"
+              className="copyInput"
+            />
+            <Button
+              buttonWidth="73px"
+              buttonHeight="40px"
+              buttonBgColor="#31C1FF"
+              buttonTitle="Copy"
+              buttonFontSize="12px"
+              buttonRadius="10px"
+              buttonMarginL="-30px"
+            />
+          </div>
           <Button
-            buttonWidth="73px"
-            buttonHeight="40px"
-            buttonBgColor="#31C1FF"
-            buttonTitle="Copy"
-            buttonFontSize="12px"
-            buttonRadius="10px"
-            buttonPosition="absolute"
-            buttonMarginL="-30px"
+            buttonTitle="DOWNLOAD"
+            buttonWidth="176px"
+            buttonHeight="47px"
+            buttonFontSize="16px"
           />
         </div>
-        <div className="copyInputBotCont">
-          <input
-            type="text"
-            placeholder="link.quikpix.converted..."
-            className="copyInput"
-          />
-          <Button
-            buttonWidth="73px"
-            buttonHeight="40px"
-            buttonBgColor="#31C1FF"
-            buttonTitle="Copy"
-            buttonFontSize="12px"
-            buttonRadius="10px"
-            buttonMarginL="-30px"
-          />
-        </div>
-        <Button
-          buttonTitle="DOWNLOAD"
-          buttonWidth="176px"
-          buttonHeight="47px"
-          buttonFontSize="16px"
-        />
       </div>
     </div>
   );
@@ -55,7 +70,10 @@ const ResultsCard = ({ innerImg, dimNum }) => {
 
 ResultsCard.defaultProps = {
   dimNum: "",
-  innerImg: ""
+  innerImg: "",
+  resultsCardIcon:
+    "https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png",
+  resultsCardTitle: "Default Title"
 };
 
 export default ResultsCard;
