2bafbc811a94bfe38da388d68c6cc21f80103fe3
mattnazari <matthew.nazari8@gmail.com>
2020-02-17 19:37:48 -0800

    locked and checked cards complete

--- src/comps/Card/index.js
+++ src/comps/Card/index.js
@@ -1,29 +1,76 @@
 import React from "react";
 
-const Card = ({ imgWidth, imgHeight, titleTxt, dimTxt, backgroundimg }) => {
+const Card = ({
+  imgWidth,
+  imgHeight,
+  titleTxt,
+  dimTxt,
+  bgImg,
+  bgColor,
+  innerImg,
+  innerTxt,
+  innerImgW,
+  innerImgH,
+  bgOpacity,
+  innerTxtSize,
+  innerTxtMTop,
+  titleTxtOp,
+  dimTxtOp
+}) => {
   return (
     <div className="cardCont">
-      <div
-        className="image"
-        style={{
-          backgroundImage: backgroundimg,
-          width: imgWidth,
-          height: imgHeight
-        }}
-      ></div>
-      <p className="titleText">{titleTxt}</p>
-      <p className="dimText">{dimTxt}</p>
+      <div className="imageCont">
+        <div
+          className="image"
+          style={{
+            backgroundImage: bgImg,
+            width: imgWidth,
+            height: imgHeight,
+            backgroundColor: bgColor,
+            opacity: bgOpacity
+          }}
+        ></div>
+        <div
+          className="innerImg"
+          style={{
+            backgroundImage: innerImg,
+            width: innerImgW,
+            height: innerImgH
+          }}
+        ></div>
+        <p
+          className="innerTxt"
+          style={{ fontSize: innerTxtSize, marginTop: innerTxtMTop }}
+        >
+          {innerTxt}
+        </p>
+      </div>
+      <p className="titleText" style={{ opacity: titleTxtOp }}>
+        {titleTxt}
+      </p>
+      <p className="dimText" style={{ opacity: dimTxtOp }}>
+        {dimTxt}
+      </p>
     </div>
   );
 };
 
 Card.defaultProps = {
-  backgroundimg:
-    "url('https://www.rabata.org/wp-content/uploads/2018/05/dummy.png')",
+  bgImg: "url('https://www.rabata.org/wp-content/uploads/2018/05/dummy.png')",
   imgWidth: "100px",
   imgHeight: "100px",
   titleTxt: "default title text",
-  dimTxt: "default dim text"
+  dimTxt: "default dim text",
+  bgColor: "",
+  innerImg: "",
+  innerTxt: "",
+  innerImgH: "",
+  innerImgW: "",
+  bgOpacity: "",
+  innerTxtSize: "12px",
+  innerTxtMTop: "50px",
+  titleTxtOp: "",
+  dimTxtOp: ""
 };
 
 export default Card;
--- src/stories/Card.stories.js
+++ src/stories/Card.stories.js
@@ -22,3 +22,31 @@ export const ProfileCard = () => {
     />
   );
 };
+
+export const checkedCard = () => {
+  return (
+    <Card
+      bgColor="#31C1FF"
+      bgImg=""
+      innerImg="url('http://www.matthewnazari.ca/check.svg')"
+      innerImgW="50px"
+      innerImgH="50px"
+    />
+  );
+};
+
+export const lockedCard = () => {
+  return (
+    <Card
+      imgWidth="140px"
+      imgHeight="240px"
+      innerImg="url('http://www.matthewnazari.ca/lock.svg')"
+      innerImgW="50px"
+      innerImgH="50px"
+      bgOpacity="0.2"
+      innerTxt="Upgrade to unlock"
+      titleTxtOp="0.2"
+      dimTxtOp="0.2"
+    />
+  );
+};
